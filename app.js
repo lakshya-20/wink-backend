d3f4b183b168a069b9061a4337aa4c166d42d7b0
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-25 01:07:06 +0530

    data base integration

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express=require('express')
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const {mongourl}=require('./config');
+var connect=mongoose.connect(mongourl);
+connect.then((db) =>{
+  console.log('Connected correctly to mongodb');
+},(err)=>{console.log(err)});
 
 const app=express()
 const PORT=process.env.PORT ||5000
+
 
 app.use(express.json())
 
