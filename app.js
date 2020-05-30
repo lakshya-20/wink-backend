806d719903753059e967142bb593d37cda15595e
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-31 03:47:11 +0530

    socket io started

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const http = require('http');
+
+//const socketio = require('socket.io');
+const client = require('socket.io').listen(4000).sockets;
 
 
 const {mongourl}=require('./config');
@@ -10,7 +14,7 @@ connect.then((db) =>{
 
 const app=express()
 const PORT=process.env.PORT ||5000
-
+const server=http.createServer(app);
 require('./models/user')
 require('./models/conversation')
 
@@ -18,6 +22,21 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/conversations'))
 app.use(require('./routes/users'))
+
+
+client.on('connection',function(socket){
+  
+  console.log("connected")
+  socket.on('disconnect',function(){
+    console.log("disconnected")
+  })
+  socket.on('input',function(conversationId,data){
+    console.log("con",conversationId)
+    client.emit('output',conversationId,data);
+  })
+});
+
+
 
 app.listen(PORT,()=>{
     console.log("Server starting on port no: ",PORT)
