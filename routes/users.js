479504a441c4333883777b2edddf1f5d90d2f061
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-05-30 16:00:38 +0530

    minor changes in searchusers route

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,21 @@ const mongoose = require('mongoose')
 const User =  mongoose.model("User")
 
 router.post('/searchUsers',(req,res)=>{
-    let userPattern=new RegExp("^"+req.body.query)
-    User.find({name:{$regex:userPattern}})
-    .select("_id name photo")
-    .then(user=>{
-        for(var i=0;i<user.length;i++){
-            if(user[i]._id==req.body.id){
-                user.splice(i,1)
+    if(req.body.query!=''){
+        let userPattern=new RegExp("^"+req.body.query)
+        User.find({name:{$regex:userPattern}})
+        .select("_id name photo")
+        .then(user=>{
+            for(var i=0;i<user.length;i++){
+                if(user[i]._id==req.body.id){
+                    user.splice(i,1)
+                }
             }
-        }
-        res.json({user})
-    }).catch(err=>{
-        console.log(err)
-    })
+            res.json({user})
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
 })
 
 
